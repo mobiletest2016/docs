56f30136577df38470df088c16942be6adb7d96e
MobileTest2016 <mobiletest.mt2016@gmail.com>
2023-11-06 23:09:37 +0530

    .
    Signed-off-by: MobileTest2016 <mobiletest.mt2016@gmail.com>

--- Code.java
+++ Code.java
@@ -9,14 +9,14 @@ public class Code {
     }
 
     private static String dec(String filename) throws IOException {
-        String newFile = filename.replace(".pdf", "_.pdf");
+        String newFile = getNewFileName(filename);
         InputStream in = new FileInputStream(filename);
         OutputStream out = new FileOutputStream(newFile);
         int b;
-	byte[] data = new byte[1024];
-        while((b = in.read(data)) != -1) {
-	    for(int i = 0; i < b; i++)
-		    data[i]--;
+        byte[] data = new byte[1024];
+        while ((b = in.read(data)) != -1) {
+            for (int i = 0; i < b; i++)
+                data[i]--;
             out.write(data, 0, b);
         }
         out.close();
@@ -25,18 +25,25 @@ public class Code {
     }
 
     private static String enc(String filename) throws IOException {
-        String newFile = filename.replace(".pdf", "_.pdf");
+        String newFile = getNewFileName(filename);
         InputStream in = new FileInputStream(filename);
         OutputStream out = new FileOutputStream(newFile);
         int b;
-	byte[] data = new byte[1024];
-        while((b = in.read(data)) != -1) {
-	    for(int i = 0; i < b; i++)
-		    data[i]++;
+        byte[] data = new byte[1024];
+        while ((b = in.read(data)) != -1) {
+            for (int i = 0; i < b; i++)
+                data[i]++;
             out.write(data, 0, b);
         }
         out.close();
         in.close();
         return newFile;
+    }
+
+    private static String getNewFileName(String filename) {
+        String extn = filename.substring(filename.lastIndexOf('.'));
+        String filename_ = filename.substring(0, filename.lastIndexOf('.'));
+
+        return filename_ + "_" + extn;
     }
 }
